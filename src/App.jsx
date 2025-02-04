db813ea670f737eb46f3eb3081089e58124cf900
Abdullohass <abdulloh.s.gk@gmail.com>
2025-02-04 14:51:44 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello React</h1>
+      <h2>This is my first React app</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+      </ul>
+      <h2>What is React</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore exercitationem voluptate esse incidunt? Nemo porro molestias illum ex? Voluptates deleniti, perspiciatis exercitationem nesciunt mollitia dicta adipisci maxime expedita quod velit quo aut minima error temporibus architecto rerum accusamus voluptas assumenda nam? Cumque, quibusdam? Quod reiciendis dolor tempore repudiandae architecto non?</p>
+      <h2>What is Single-page application(SPA)</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut odio porro quod voluptatem numquam quia nobis minus enim minima placeat cupiditate eum, impedit eos quasi consequuntur excepturi corrupti in ullam, repellat quibusdam perspiciatis. Quidem non magni explicabo, ab necessitatibus ex atque natus qui officiis? Reprehenderit repellat a praesentium recusandae sed?</p>
+    </>
+  )
+}
+
+export default App
